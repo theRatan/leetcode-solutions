f674992374d17f0ef042712d95248ae35ff3cefb
Ratan Mahato <ratanmahato036@gmail.com>
2026-06-28 18:09:59 +0530

    Update TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,13 +1,30 @@
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
-            }
-        }
-        return new int[] {};
+        
+     int sm=0;
+     int ar[]=new int[2];
+     for(int i=0;i<nums.length-1;i++)
+     {
+         for(int j=0;j<nums.length;j++)
+         {
+             sm=nums[i]+nums[j];
+             if(sm==target)
+             {
+                 if(i==j)
+                 {
+                     
+                     continue;
+                 }
+                 else
+                 {
+                 ar[0]=i;
+                 ar[1]=j;
+                 }
+             }
+             else
+               sm=0;
+         }
+     }
+     return ar;
     }
 }
